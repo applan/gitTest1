7e889083c426715c19768d0faa2149c1885f26cb
siepe <applan_siepe@naver.com>
2019-03-26 14:11:47 +0900

    master branch

--- Variable.java
+++ Variable.java
@@ -1,7 +1,8 @@
 public class Variable{
   public static void main(String[] args){
             System.out.print("hotfix branch");
-
+            System.out.print("master branch");
+            System.out.print("mas");
 
 
  }
